25eb461c76c3c5d51da78045bd681e0b549de348
Abbas Moghadasi <abm@schaeffer-ag.de>
2022-04-21 17:52:06 +0200

    soluition 1

---
+++ object-sample-solution.js
@@ -0,0 +1,8 @@
+const { properties: elementProps } = scProps;
+const coordinate = elementProps
+  .filter((prop) => {
+    const { label } = prop;
+    return label === "X" || label === "Y";
+  })
+  .map((prop) => +[prop.value.match(/\d+\.\d+/)]);
+const [x, y] = coordinate;
