f26132a1639f6af637450d3fb9015329df99d6bb
Abbas Moghadasi <abm@schaeffer-ag.de>
2022-04-21 17:49:02 +0200

    test object

---
+++ object-sample.js
@@ -0,0 +1,59 @@
+var testObj = {
+  id: 6403,
+  version: 31453,
+  panel_version: 4158,
+  element_ids: [136395926],
+  properties: [
+    {
+      "base-type": "string",
+      label: "X",
+      value: "-11.6 mm",
+    },
+    {
+      "base-type": "string",
+      label: "Y",
+      value: "36.81 mm",
+    },
+    {
+      "base-type": "string",
+      label: "Diameter",
+      value: "18.04 mm",
+    },
+    {
+      "base-type": "string",
+      label: "Countersunk",
+      value: "Keine",
+    },
+    {
+      "base-type": "string",
+      label: "Thread",
+      value: "Keines",
+    },
+    {
+      "base-type": "string",
+      label: "BlindHole",
+      value: "0",
+    },
+    {
+      "base-type": "string",
+      label: "HelpElement",
+      value: "0",
+    },
+    {
+      "base-type": "string",
+      label: "ObjectID",
+      value: "",
+    },
+    {
+      "base-type": "string",
+      label: "Bevel/FrontShape",
+      value: "Keine Bearbeitung",
+    },
+    {
+      "base-type": "string",
+      label: "Bevel/RevShape",
+      value: "Keine Bearbeitung",
+    },
+  ],
+  previous_version: 31452,
+};
