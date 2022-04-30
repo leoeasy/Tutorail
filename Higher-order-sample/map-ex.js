cf604a0f6bcec39ff2264f94f972eb248e0625f4
Abbas Moghadasi <abm@schaeffer-ag.de>
2022-04-30 21:01:54 +0200

    ex4

--- Higher-order-sample/map-ex.js
+++ Higher-order-sample/map-ex.js
@@ -4,6 +4,9 @@ var arr1 = [1, 2, 3];
 var input = [1, 2, 3, 4, 5];
 //   [ 1, 4, 9, 16, 25]
 
+var birthYear = [1975, 1997, 2002, 1995, 1985];
+// [47, 25, 20, 27, 37]
+
 var kvArray = [
   { key: 1, value: 10 },
   { key: 2, value: 20 },
