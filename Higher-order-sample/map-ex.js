343d7f36dbdc4dbaf7983803bf86888dc4998f8c
Abbas Moghadasi <abm@schaeffer-ag.de>
2022-04-30 20:59:40 +0200

    ex3

---
+++ Higher-order-sample/map-ex.js
@@ -0,0 +1,13 @@
+var arr1 = [1, 2, 3];
+////  [ 2, 4, 6 ]
+
+var input = [1, 2, 3, 4, 5];
+//   [ 1, 4, 9, 16, 25]
+
+var kvArray = [
+  { key: 1, value: 10 },
+  { key: 2, value: 20 },
+  { key: 3, value: 30 },
+];
+
+// reformattedArray is now [{1: 10}, {2: 20}, {3: 30}],
