acef3e3760893cc21b2f4caa1a39e1a33747314f
Abbas Moghadasi <abm@schaeffer-ag.de>
2022-04-30 20:26:12 +0200

    sample2

---
+++ Higher-order-sample/object-sample2.js
@@ -0,0 +1,7 @@
+var winners = [
+  { name: "Mario", time: 110 },
+  { name: "Waluigi", time: 115 },
+  { name: "Toad", time: 116 },
+  { name: "Yoshi", time: 124 },
+  { name: "Bowser", time: 129 },
+];
