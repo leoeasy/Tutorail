d9600b45becad378f90d58f63f00ce8f0c46bda3
Abbas Moghadasi <abm@schaeffer-ag.de>
2022-06-29 21:45:02 +0200

    add new api props sample

---
+++ Higher-order-sample/propertieSample.js
@@ -0,0 +1,55 @@
+var memberProps = {
+  member_ids: [3672883562],
+  member_properties: [
+    {
+      X: "33.47 mm",
+    },
+    {
+      Y: "24.47 mm",
+    },
+    {
+      Shape: "",
+    },
+    {
+      Diameter: "4.84 mm",
+    },
+    {
+      Depth: "2.00 mm",
+    },
+    {
+      AutoTool: "1",
+    },
+    {
+      ReverseSide: "0",
+    },
+    {
+      HelpElement: "0",
+    },
+    {
+      ObjectID: "",
+    },
+    {
+      "Bevel/FrontShape": "Keine Bearbeitung",
+    },
+  ],
+  properties: [],
+  document_version: 10,
+};
+
+//member_properties
+var outmember_properties = {
+  X: "50.90 mm",
+  Y: "36.73 mm",
+  Height: "8.47 mm",
+  Width: "10.82 mm",
+  CornerR: "1.50 mm",
+  Angle: "",
+  AutoTool: "1",
+  Triangle: "0",
+  AngleLeft: "",
+  AngleRight: "",
+  HelpElement: "0",
+  ObjectID: "",
+  "Bevel/FrontShape": "Keine Bearbeitung",
+  "Bevel/RevShape": "Keine Bearbeitung",
+};
