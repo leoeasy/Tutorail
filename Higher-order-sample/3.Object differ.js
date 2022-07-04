cf74cb181d51c671b1489bb2ee9cdd7b14df4fc7
Abbas Moghadasi <abm@schaeffer-ag.de>
2022-07-04 10:20:02 +0200

    working with objects

--- Higher-order-sample/3.Object differ.js
+++ Higher-order-sample/3.Object differ.js
@@ -20,19 +20,54 @@ let diff = Object.keys(o2).reduce((diff, key) => {
 }, {});
 // {one: 7, four: 4}
 
+//solution1
 Object.entries(o1).map((z) => {
   let obj = { [z[0]]: z[1] };
   return obj;
 });
 
-// [
-//   {
-//     "one": 1
-// },
-// {
-//     "two": 2
-// },
-// {
-//     "three": 3
-// }
-// ]
+/**[
+  {
+      "one": 1
+  },
+  {
+      "two": 2
+  },
+  {
+      "three": 3
+  }
+]
+*/
+
+//solution2
+Object.keys(o1).map((k) => ({ [k]: o1[k] }));
+
+/**[
+  {
+      "one": 1
+  },
+  {
+      "two": 2
+  },
+  {
+      "three": 3
+  }
+]
+*/
+
+// solution 3
+Object.entries(o1).map((prop) => {
+  const mp = new Map([prop]);
+  return Object.fromEntries(mp);
+});
+/**[
+    {
+        "one": 1
+    },
+    {
+        "two": 2
+    },
+    {
+        "three": 3
+    }
+] */
